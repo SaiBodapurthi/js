4cb59d07ecc130128b911bd152f3ebfe08648d30
sai <saipavanbodapurthi@gmail.com>
2024-03-25 12:44:02 +0530

    variables

---
+++ variable.js
@@ -0,0 +1,9 @@
+const sRollno = 1121
+let sName="sai"
+var sEmail="sai@google.com"
+sSchool = "SRU"
+console.table([ sRollno, sName, sEmail, sSchool])
+sName="pavan"
+sEmail="pavan@sai.in"
+sSchool="SRIX"
+console.table([ sRollno, sName, sEmail, sSchool])
